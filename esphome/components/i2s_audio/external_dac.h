671d8f5124f5abd0b2d37542a50565ab830cb3c5
luketcus <luketcus@gmail.com>
2024-08-24 10:29:56 -0300

    Update external_dac.h

--- esphome/components/i2s_audio/external_dac.h
+++ esphome/components/i2s_audio/external_dac.h
@@ -10,60 +10,80 @@
 
 namespace esphome {
 namespace i2s_audio {
+#define AC101_ADDR 0x1A // Device address
 
-#define CHIP_AUDIO_RS		0x00
-#define PLL_CTRL1			0x01
-#define PLL_CTRL2			0x02
-#define SYSCLK_CTRL			0x03
-#define MOD_CLK_ENA			0x04
-#define MOD_RST_CTRL		0x05
-#define I2S_SR_CTRL			0x06
-#define I2S1LCK_CTRL		0x10
-#define I2S1_SDOUT_CTRL		0x11
-#define I2S1_SDIN_CTRL		0x12
-#define I2S1_MXR_SRC		0x13
-#define I2S1_VOL_CTRL1		0x14
-#define I2S1_VOL_CTRL2		0x15
-#define I2S1_VOL_CTRL3		0x16
-#define I2S1_VOL_CTRL4		0x17
-#define I2S1_MXR_GAIN		0x18
-#define ADC_DIG_CTRL		0x40
-#define ADC_VOL_CTRL		0x41
-#define HMIC_CTRL1			0x44
-#define HMIC_CTRL2			0x45
-#define HMIC_STATUS			0x46
-#define DAC_DIG_CTRL		0x48
-#define DAC_VOL_CTRL		0x49
-#define DAC_MXR_SRC			0x4c
-#define DAC_MXR_GAIN		0x4d
-#define ADC_APC_CTRL		0x50
-#define ADC_SRC				0x51
-#define ADC_SRCBST_CTRL		0x52
-#define OMIXER_DACA_CTRL	0x53
-#define OMIXER_SR			0x54
-#define OMIXER_BST1_CTRL	0x55
-#define HPOUT_CTRL			0x56
-#define SPKOUT_CTRL			0x58
-#define AC_DAC_DAPCTRL		0xa0
-#define AC_DAC_DAPHHPFC 	0xa1
-#define AC_DAC_DAPLHPFC 	0xa2
-#define AC_DAC_DAPLHAVC 	0xa3
-#define AC_DAC_DAPLLAVC 	0xa4
-#define AC_DAC_DAPRHAVC 	0xa5
-#define AC_DAC_DAPRLAVC 	0xa6
-#define AC_DAC_DAPHGDEC 	0xa7
-#define AC_DAC_DAPLGDEC 	0xa8
-#define AC_DAC_DAPHGATC 	0xa9
-#define AC_DAC_DAPLGATC 	0xaa
-#define AC_DAC_DAPHETHD 	0xab
-#define AC_DAC_DAPLETHD 	0xac
-#define AC_DAC_DAPHGKPA 	0xad
-#define AC_DAC_DAPLGKPA 	0xae
-#define AC_DAC_DAPHGOPA 	0xaf
-#define AC_DAC_DAPLGOPA 	0xb0
-#define AC_DAC_DAPOPT   	0xb1
-#define DAC_DAP_ENA     	0xb5
+static const uint8_t AC101_CHIP_AUDIO_RS = 0x00;
+static const uint8_t AC101_PLL_CTRL1 = 0x01;
+static const uint8_t AC101_PLL_CTRL2 = 0x02;
+static const uint8_t AC101_SYSCLK_CTRL = 0x03;
+static const uint8_t AC101_MOD_CLK_ENA = 0x04;
+static const uint8_t AC101_MOD_RST_CTRL = 0x05;
+static const uint8_t AC101_I2S_SR_CTRL = 0x06;
+static const uint8_t AC101_I2S1LCK_CTRL = 0x10;
+static const uint8_t AC101_I2S1_SDOUT_CTRL = 0x11;
+static const uint8_t AC101_I2S1_SDIN_CTRL = 0x12;
+static const uint8_t AC101_I2S1_MXR_SRC = 0x13;
+static const uint8_t AC101_I2S1_VOL_CTRL1 = 0x14;
+static const uint8_t AC101_I2S1_VOL_CTRL2 = 0x15;
+static const uint8_t AC101_I2S1_VOL_CTRL3 = 0x16;
+static const uint8_t AC101_I2S1_VOL_CTRL4 = 0x17;
+static const uint8_t AC101_I2S1_MXR_GAIN = 0x18;
+static const uint8_t AC101_ADC_DIG_CTRL = 0x40;
+static const uint8_t AC101_ADC_VOL_CTRL = 0x41;
+static const uint8_t AC101_HMIC_CTRL1 = 0x44;
+static const uint8_t AC101_HMIC_CTRL2 = 0x45;
+static const uint8_t AC101_HMIC_STATUS = 0x46;
+static const uint8_t AC101_DAC_DIG_CTRL = 0x48;
+static const uint8_t AC101_DAC_VOL_CTRL = 0x49;
+static const uint8_t AC101_DAC_MXR_SRC = 0x4C;
+static const uint8_t AC101_DAC_MXR_GAIN = 0x4D;
+static const uint8_t AC101_ADC_APC_CTRL = 0x50;
+static const uint8_t AC101_ADC_SRC = 0x51;
+static const uint8_t AC101_ADC_SRCBST_CTRL = 0x52;
+static const uint8_t AC101_OMIXER_DACA_CTRL = 0x53;
+static const uint8_t AC101_OMIXER_SR = 0x54;
+static const uint8_t AC101_OMIXER_BST1_CTRL = 0x55;
+static const uint8_t AC101_HPOUT_CTRL = 0x56;
+static const uint8_t AC101_SPKOUT_CTRL = 0x58;
+static const uint8_t AC101_AC_DAC_DAPCTRL = 0xA0;
+static const uint8_t AC101_AC_DAC_DAPHHPFC = 0xA1;
+static const uint8_t AC101_AC_DAC_DAPLHPFC = 0xA2;
+static const uint8_t AC101_AC_DAC_DAPLHAVC = 0xA3;
+static const uint8_t AC101_AC_DAC_DAPLLAVC = 0xA4;
+static const uint8_t AC101_AC_DAC_DAPRHAVC = 0xA5;
+static const uint8_t AC101_AC_DAC_DAPRLAVC = 0xA6;
+static const uint8_t AC101_AC_DAC_DAPHGDEC = 0xA7;
+static const uint8_t AC101_AC_DAC_DAPLGDEC = 0xA8;
+static const uint8_t AC101_AC_DAC_DAPHGATC = 0xA9;
+static const uint8_t AC101_AC_DAC_DAPLGATC = 0xAA;
+static const uint8_t AC101_AC_DAC_DAPHETHD = 0xAB;
+static const uint8_t AC101_AC_DAC_DAPLETHD = 0xAC;
+static const uint8_t AC101_AC_DAC_DAPHGKPA = 0xAD;
+static const uint8_t AC101_AC_DAC_DAPLGKPA = 0xAE;
+static const uint8_t AC101_AC_DAC_DAPHGOPA = 0xAF;
+static const uint8_t AC101_AC_DAC_DAPLGOPA = 0xB0;
+static const uint8_t AC101_AC_DAC_DAPOPT = 0xB1;
+static const uint8_t AC101_DAC_DAP_ENA = 0xB5;
 
+static const uint8_t AC101_REGS[] = {
+    AC101_CHIP_AUDIO_RS,    AC101_PLL_CTRL1,        AC101_PLL_CTRL2,
+    AC101_SYSCLK_CTRL,      AC101_MOD_CLK_ENA,      AC101_MOD_RST_CTRL,
+    AC101_I2S_SR_CTRL,      AC101_I2S1LCK_CTRL,     AC101_I2S1_SDOUT_CTRL,
+    AC101_I2S1_SDIN_CTRL,   AC101_I2S1_MXR_SRC,     AC101_I2S1_VOL_CTRL1,
+    AC101_I2S1_VOL_CTRL2,   AC101_I2S1_VOL_CTRL3,   AC101_I2S1_VOL_CTRL4,
+    AC101_I2S1_MXR_GAIN,    AC101_ADC_DIG_CTRL,     AC101_ADC_VOL_CTRL,
+    AC101_HMIC_CTRL1,       AC101_HMIC_CTRL2,       AC101_HMIC_STATUS,
+    AC101_DAC_DIG_CTRL,     AC101_DAC_VOL_CTRL,     AC101_DAC_MXR_SRC,
+    AC101_DAC_MXR_GAIN,     AC101_ADC_APC_CTRL,     AC101_ADC_SRC,
+    AC101_ADC_SRCBST_CTRL,  AC101_OMIXER_DACA_CTRL, AC101_OMIXER_SR,
+    AC101_OMIXER_BST1_CTRL, AC101_HPOUT_CTRL,       AC101_SPKOUT_CTRL,
+    AC101_AC_DAC_DAPCTRL,   AC101_AC_DAC_DAPHHPFC,  AC101_AC_DAC_DAPLHPFC,
+    AC101_AC_DAC_DAPLHAVC,  AC101_AC_DAC_DAPLLAVC,  AC101_AC_DAC_DAPRHAVC,
+    AC101_AC_DAC_DAPRLAVC,  AC101_AC_DAC_DAPHGDEC,  AC101_AC_DAC_DAPLGDEC,
+    AC101_AC_DAC_DAPHGATC,  AC101_AC_DAC_DAPLGATC,  AC101_AC_DAC_DAPHETHD,
+    AC101_AC_DAC_DAPLETHD,  AC101_AC_DAC_DAPHGKPA,  AC101_AC_DAC_DAPLGKPA,
+    AC101_AC_DAC_DAPHGOPA,  AC101_AC_DAC_DAPLGOPA,  AC101_AC_DAC_DAPOPT,
+    AC101_DAC_DAP_ENA};
 class ExternalDAC : public i2c::I2CDevice {
 public:
   virtual bool init_device() = 0;
@@ -96,7 +116,9 @@ class AC101 : public ExternalDAC{
   bool apply_i2s_settings(const i2s_driver_config_t&  i2s_cfg) override;
   bool set_mute_audio( bool mute );
   bool set_volume( float volume );
-
+private:
+  bool WriteReg(uint8_t reg, uint16_t val);
+  bool ReadReg(uint8_t reg, uint16_t *val);
 };
 }
 }
